1b77f98f9e047013e8a66b26b8386af94b8c07a4
VitoriaAmorim <vitoriajuliana118@gmail.com>
2022-11-23 16:04:09 -0300

    23-11-22

---
+++ JavaScript/ListaAluno.js
@@ -0,0 +1,15 @@
+let nome = 'Jake'
+
+switch (nome){
+    case 'Jessy':
+        console.log ('Jessy - 25 anos')
+        break
+        case 'Lilly':
+            console.log ('Lilly - 22 anos')
+            break
+            case 'Jake':
+                console.log ('Jake - 26 anos')
+                break
+                default:
+                    console.log ('insira o nome corretamente.')
+}
---
+++ JavaScript/exemplo.js
@@ -0,0 +1,19 @@
+let ItemCompra = 'abacate'
+
+switch (ItemCompra){
+    case 'abacate':
+        console.log ('abacate kg: R$2,99')
+        break
+        case 'tomate':
+            console.log('tomate kg: R$:1,99')
+            break
+            case 'alface':
+                console.log('alface unidade: R$:4,99')
+                break
+                case 'cenoura':
+                    console.log('cenoura kg: R$:0,99')
+                    break
+                    default:
+                        console.log('Item avaliado')
+                        break
+}
