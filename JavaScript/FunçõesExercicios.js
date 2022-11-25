fd75a3244cb44a2446e37171d61f20400ab4e19c
VitoriaAmorim <vitoriajuliana118@gmail.com>
2022-11-25 16:06:30 -0300

    25-11-22

---
+++ JavaScript/FunçõesExercicios.js
@@ -0,0 +1,28 @@
+// Função para somar dois números.
+function calcular (n1,n2)//Argumentos
+{
+    console.log (n1+n2)
+}
+calcular (10,10)
+
+// Função para calcular a divisão de dois números.
+function div (n1,n2){
+    console.log (n1/n2)
+}
+div (100,10)
+
+// Função para calcular a área de um triângulo.
+function area (b,h){
+    console.log (b*h/2)
+}
+area (20,10)
+
+// Função para exibir mensagem de erro caso idade seja menor que 18.
+function mensagem (idade){
+    if (mensagem < 18){
+        console.log('ERROR')
+    }else {
+        console.log ('OK')
+    }
+}
+mensagem (18)
---
+++ JavaScript/funções.js
@@ -0,0 +1,7 @@
+function curso (nome,tempo){
+    console.log (`o nome do curso é: ${nome} e sua duração é de: ${tempo} meses.`)
+}
+curso ('curso')
+curso ('Favela Ware','6')
+
+
